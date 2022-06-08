39c54f18713479cbed1002bfb78eba4ca0ddb5e8
Buzz88-Dev <omarbuzzoni88@gmail.com>
2022-06-08 11:18:56 +0200

    ended

---
+++ src/components/Hero.vue
@@ -0,0 +1,14 @@
+<template>
+  <img src="../assets/game.png" alt="">
+</template>
+
+<script>
+export default {
+  name: 'FirstImage',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
